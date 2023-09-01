d097c8f5a7d786e016d5c11be30822fdac12c26e
Amr Aly <amr83160@gmail.com>
2023-09-01 10:05:50 -0700

    edited

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,26 +1,14 @@
 #include <stdio.h>
-#include "holberton.h"
 
 /**
- * main - Prints the number of arguments passed into main
- * @argc: Number of command line arguments
- * @argv: Array name
+ * main - prints its name, followed by a new line
+ * @argc: n args
+ * @argv: arr args
  * Return: 0
  */
-
-int main(int argc, char *argv[])
+int main(int argc, char *argv[] __attribute__((unused)))
 {
-	int i;
-
-	if (argc == 1)
+	if (argc > 0)
 		printf("%d\n", argc - 1);
-	else
-	{
-		for (i = 0; *argv; i++, argv++)
-			;
-
-		printf("%d\n", i - 1);
-	}
-
 	return (0);
 }
