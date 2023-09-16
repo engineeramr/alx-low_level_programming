4ae5249df41d88e89447d0f0255e4e0a284a7399
Amr Aly <amr83160@gmail.com>
2023-09-15 18:27:16 -0700

    by amr alli

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: prints all possible different combinations of three digits
+ *
+ * Return: 0
+ *
+*/
+int main(void)
+{
+int digit1 = 0, digit2, digit3;
+while (digit1 <= 9)
+{
+digit2 = 0;
+while (digit2 <= 9)
+{
+digit3 = 0;
+while (digit3 <= 9)
+{
+putchar(digit1 + 48);
+putchar(digit2 + 48);
+putchar(digit3 + 48);
+}
+digit3++;
+}
+digit2++;
+}
+digit1++;
+putchar('\n');
+return (0);
+}
