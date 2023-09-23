7a9ac624496f0e5cae69dbb98df83a844815f3a5
Amr Aly <amr83160@gmail.com>
2023-09-22 18:37:25 -0700

    linked lists

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/**
+ * _strlen - returns the length of a string
+ *
+ * @s: the string whose length to check
+ *
+ * Return: integer lenght of string
+*/
+size_t print_list(const list_t *h)
+{
+	size_t n = 0;
+
+	while (h)
+	{
+		if (!h->str)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+		h = h->next;
+		n++;
+	}
+
+	return (n);
+}
